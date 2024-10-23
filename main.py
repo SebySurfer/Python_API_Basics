4dd95eb1ebea86673c0830a7341fd4383283b598
SebySurfer <sebysurfer@gmail.com>
2024-10-23 16:11:27 -0600

    Lesson 2
    Raising and handling errors

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 import random
 
@@ -35,11 +35,36 @@ def create_item(item: str): #Create a method (by default is async) of any name t
 # Terminal:
 # curl -X GET http://127.0.0.1:8000/myPostPath/INTEGER
 @app.get("/myPostPath/{an_item_id}") #Based on the same path to access your list, which is just a best practice (not necessary), then add another path to specify a variable to use for the method
-def get_item(an_item_id: int) -> str:
+def get_item(an_item_id: int) -> str: #Really the 'an_item_id' or using 'id' is just a conventional way to name it (good practice). You can also name it 'Var', or whatever you like
     item = itemsList[an_item_id]
     return item
 
 #Note: You are only using reference to the actual memory of your program
+
+
+# Lesson 2: Raising Errors
+# Whenever we encoutner errors, a lot of times it is logical to understand why an error occured (Ex: using str instead of int)
+# But its not very useful to see that the response doesnt specify why, leaving users question what went wrong
+# So we need to see how to rasise very useful errr messages, so that when your developing the app, you can debug it and figure out what went wrong.
+
+# How to configure these errors:
+# There's a universal set of http response codes that you can use and everybody will understand
+# Link: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+# We will import 'HTTPException' from FastAPI
+
+#Lets replicate the same get request from the previous api, but with the error exception implemented
+
+# Terminal:
+# curl -X GET http://127.0.0.1:8000/myErrorExample/INTEGER
+@app.get("/myErrorExample/{index}")
+def get_item(index: int) -> str:
+    if index < len(itemsList): #If you pass an integer lower than the total length, its truthy, if not its falsy
+        return itemsList[index]
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {index} is too big senpai") # HTTPException has the strict field 'status_code' implemented, and has an optional field 'detail' to display your own message
+        #The exception code you can find in the universal glossary,
+        # where you can find the standard that meets the criteria which will result in error
+
 
 
 
