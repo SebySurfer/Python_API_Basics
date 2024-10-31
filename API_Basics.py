852d30fe24627600d3e92910d28f168b0dad20ec
SebySurfer <sebysurfer@gmail.com>
2024-10-31 15:23:47 -0600

    Basic CRUD

--- API_Basics.py
+++ API_Basics.py
@@ -29,9 +29,59 @@
     # Ensure the correct data is provided
 
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field #Field allows you to add metadata and customize how fields in your Pydantic models behave
+from typing import List, Optional #Optional allows for it to be
+from uuid import UUID, uuid4  # Stands for "Unique identifier", where it always guarantees a unique ID
 
 app = FastAPI()
 
 
+# This class will represent the objects that we'll be pasing around this API
+class Task(BaseModel):
+    id: UUID = Field(default_factory=uuid4)  # Generates a new UUID for each instance
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
 
+tasksList = [] #Techncally you would connect this to a real database
+
+# Create
+@app.post("/tasks/", response_model=Task)
+def create_task(task: Task):
+    tasksList.append(task)
+    return task
+
+#Read
+@app.get("/tasks/", response_model=List[Task])
+def read_task():
+    return tasksList
+
+
+#Tip = Doesn't matter if you have the same endpoint, as long as the task request is different
+
+#Read
+@app.get("/tasks/{task_id}", response_model=Task)
+def read_task(task_id: UUID):
+    for task in tasksList:
+        if task.id == task_id:
+            return task
+    raise HTTPException(status_code=404, detail="Task not found stuped")
+
+# Update
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: UUID, newTask: Task):
+    for task in tasksList:
+        if task.id == task_id:
+            task = newTask
+            return tasksList
+    raise HTTPException(status_code=404, detail="Task not updated cuz not found stuped")
+
+# Delete
+@app.delete("/tasks/{task_id}", response_model=Task)
+def delete_task(task_id: UUID):
+    for task in tasksList:
+        if task.id == task_id:
+            tasksList.remove(task)
+            return tasksList
+    raise HTTPException(status_code=404, detail="Task not deleted cuz not found stuped")
